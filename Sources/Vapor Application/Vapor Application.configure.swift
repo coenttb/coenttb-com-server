4e86abf9265651415e89b8b33fda64fcdcd68dc4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-28 07:49:32 +0200

    re-insert JWT

--- Sources/Vapor Application/Vapor Application.configure.swift
+++ Sources/Vapor Application/Vapor Application.configure.swift
@@ -5,6 +5,7 @@ import Coenttb_Server
 import Coenttb_Vapor
 import Fluent
 import FluentPostgresDriver
+import JWT
 import Queues
 import QueuesFluentDriver
 import Server_Client
@@ -14,35 +15,35 @@ import Server_Models
 
 extension Application {
     package static func configure(app: Vapor.Application) async throws {
-
+        
         // necessary to set here because $0.color has a liveValue default
         prepareDependencies {
             $0.color = .coenttb
         }
-
+        
         Application.preloadStaticResources()
-
+        
         @Dependency(\.envVars) var envVars
-
+        
         app.environment = .init(envVarsEnvironment: envVars.appEnv)
-
+        
         app.databases.use(.postgres, as: .psql )
-
+        
         [any Migration].allCases.forEach { app.migrations.add($0) }
-
+        
         [any AsyncCommand].allCases.forEach { app.asyncCommands.use($0.0, as: $0.1) }
-
+        
         app.migrations.add(JobMetadataMigrate())
-
+        
         if envVars.appEnv == .development {
             try await app.autoRevert()
         }
-
+        
         try await app.autoMigrate()
-
+        
         app.queues.use(.fluent())
         try app.queues.startInProcessJobs(on: .default)
-
+        
         try await Coenttb_Vapor.Application.configure(
             application: app,
             httpsRedirect: envVars.httpsRedirect,
@@ -50,11 +51,18 @@ extension Application {
             allowedInsecureHosts: envVars.allowedInsecureHosts,
             baseUrl: envVars.baseUrl
         )
-
+        
+        if let jwtPublicKey = envVars.jwtPublicKey {
+            try await app.jwt.keys.add(ecdsa: ES256PublicKey(pem: jwtPublicKey))
+        } else {
+            @Dependency(\.logger) var logger
+            logger.warning("JWT public key not set")
+        }
+        
         app.middleware.use(Identity.Consumer.Middleware())
-
+        
         @Dependency(\.coenttb.website.router) var router
-
+        
         app.mount(router, use: Route.response)
     }
 }
