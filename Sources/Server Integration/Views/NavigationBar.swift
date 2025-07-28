87a8b3bc2d53655d0583f445fec32420c5313a2b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-28 07:41:55 +0200

    remove identity-related code

--- Sources/Server Integration/Views/NavigationBar.swift
+++ Sources/Server Integration/Views/NavigationBar.swift
@@ -49,39 +49,7 @@ package struct CoenttbNavigationBar: HTML {
 
             },
             trailingNavItems: {
-                ul {
-                    HTMLGroup {
-                        if currentUser?.authenticated == true {
-                            li {
-                                CircleIconButton(
-                                    icon: .init(icon: "cog", size: .large),
-                                    color: .branding.primary,
-                                    href: .init(serverRouter.href(for: .account(.settings(.index)))),
-                                    buttonSize: .rem(2.5)
-                                )
-                            }
-
-                        } else {
-//                            li {
-//                                div {
-//                                loginButton
-//                                }
-//                                .display(.inlineBlock)
-//                            }
-
-                            li {
-                                div {
-                                    subscribeButton
-                                }
-                                .display(.inlineBlock)
-                            }
-                        }
-                    }
-                    .display(.inline)
-                    .padding(left: .rem(1), pseudo: .not(.firstChild))
-                }
-                .listStyle(.reset)
-                .display(Display.none, media: .mobile)
+                HTMLEmpty()
             },
             mobileNavItems: {
                 ul {
