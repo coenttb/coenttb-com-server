cecd0eeae2819dc7e0a19e3a3c0cdf638f1f628b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-30 22:38:23 +0200

    run swiftlint

--- Sources/Server Integration/Dependencies/Logger.swift
+++ Sources/Server Integration/Dependencies/Logger.swift
@@ -5,8 +5,8 @@
 //  Created by Coen ten Thije Boonkkamp on 12/03/2025.
 //
 
-import Coenttb_Server_Dependencies
 import Coenttb_Server
+import Coenttb_Server_Dependencies
 import Foundation
 import Logging
 
